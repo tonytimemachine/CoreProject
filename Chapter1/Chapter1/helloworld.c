9966fe19b9d9d563111fc97dfdb563d41f9a4401
刘光磊 <18601767221@163.com>
2017-08-10 18:29:11 +0800

    hello world  add comments

--- Chapter1/Chapter1/helloworld.c
+++ Chapter1/Chapter1/helloworld.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*****************************
+作者:刘光磊
+联系方式:18601767221@163.com
+创作日期:2017/8/10/ 18:24
+修改日志:
+*****************************/
 void main() {
 
 	printf("Hello World In Window10 With Visual Studio 2017 Enterprise");
